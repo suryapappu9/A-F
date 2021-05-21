28caf1311b8d68265a4e0b52305de050d6a150c5
P Surya <Psuryasfd@gmail.com>
2021-05-21 21:16:35 +0530

    init commit

--- exercise-2/js/app.js
+++ exercise-2/js/app.js
@@ -1,92 +1,235 @@
-var taskInput = document.getElementById("new-task");
-var addButton = document.getElementsByTagName("button")[0];
-var incompleteTasksHolder = document.getElementById("incomplete-tasks");
-var completedTasksHolder = document.getElementById("completed-tasks");
-
-var createNewTaskElement = function(taskString, arr) {
-  listItem = document.createElement("li");
-  checkBox = document.createElement("input");
-  label = document.createElement("label");
-  editInput = document.createElement("input");
-  editButton = document.createElement("button");
-  deleteButton = document.createElement("button");
-
-  checkBox.type = "checkbox";
-  editInput.type = "text";
-  editButton.innerText = "Edit";
-  editButton.className = "edit";
-  deleteButton.innerText = "Delete";
-  deleteButton.className = "delete";
-  label.innerText = taskString;
-
-  listItem.appendChild(checkBox);
-  listItem.appendChild(label);
-  listItem.appendChild(editInput);
-  listItem.appendChild(editButton);
-  listItem.appendChild(deleteButton);
-
-  return listItem;
-};
-
-var addTask = function () {
-  var listItemName = taskInput.value || "New Item"
-  listItem = createNewTaskElement(listItemName)
-  incompleteTasksHolder.appendChild(listItem)
-  bindTaskEvents(listItem, taskCompleted)
-  taskInput.value = "";
-};
-
-var editTask = function () {
-  var listItem = this.parentNode;
-  var editInput = listItem.querySelectorAll("input[type=text")[0];
-  var label = listItem.querySelector("label");
-  var button = listItem.getElementsByTagName("button")[0];
-
-  var containsClass = listItem.classList.contains("editMode");
-  if (containsClass) {
-      label.innerText = editInput.value
-      button.innerText = "Edit";
+(function () {
+  if (window.performance.now) {
+    console.log("Using high performance timer");
+    getTimestamp = function () {
+      return window.performance.now();
+    };
   } else {
-     editInput.value = label.innerText
-     button.innerText = "Save";
+    if (window.performance.webkitNow) {
+      console.log("Using webkit high performance timer");
+      getTimestamp = function () {
+        return window.performance.webkitNow();
+      };
+    } else {
+      console.log("Using low performance timer");
+      getTimestamp = function () {
+        return new Date().getTime() + "_" + Math.floor(Math.random() * 10);
+      };
+    }
   }
-  
-  listItem.classList.toggle("editMode");
-};
 
-var deleteTask = function (el) {
-  var listItem = this.parentNode;
-  var ul = listItem.parentNode;
-  ul.removeChild(listItem);
-};
+  const getData = function () {
+    const data = localStorage.getItem("todo_data");
+    if (data && data.length) {
+      return JSON.parse(data);
+    } else {
+      return {
+        pending: [
+          {
+            id: "task_" + getTimestamp(),
+            name: "Pay Bills",
+            inEditMode: false,
+          },
+          {
+            id: "task_" + getTimestamp(),
+            name: "Go Shopping",
+            inEditMode: true,
+          },
+        ],
+        completed: [
+          {
+            id: "task_" + getTimestamp(),
+            name: "See the Doctor",
+            inEditMode: false,
+          },
+        ],
+      };
+    }
+  };
 
-var taskCompleted = function (el) {
-  var listItem = this.parentNode;
-  completedTasksHolder.appendChild(listItem);
-  bindTaskEvents(listItem, taskIncomplete);
-};
+  const todoData = getData();  
+  const taskInput = document.getElementById("new-task");
+  const addButton = document.getElementsByTagName("button")[0];
+  const incompleteTasksHolder = document.getElementById("incomplete-tasks");
+  const completedTasksHolder = document.getElementById("completed-tasks");
 
-var taskIncomplete = function() {
-  var listItem = this.parentNode;
-  incompleteTasksHolder.appendChild(listItem);
-  bindTaskEvents(listItem, taskCompleted);
-};
+  const saveData = function () {
+    localStorage.setItem("todo_data", JSON.stringify(todoData));
+  };
 
-var bindTaskEvents = function(taskListItem, checkBoxEventHandler, cb) {
-  var checkBox = taskListItem.querySelectorAll("input[type=checkbox]")[0];
-  var editButton = taskListItem.querySelectorAll("button.edit")[0];
-  var deleteButton = taskListItem.querySelectorAll("button.delete")[0];
-  editButton.onclick = editTask;
-  deleteButton.onclick = deleteTask;
-  checkBox.onchange = checkBoxEventHandler;
-};
+  const createNewTaskElement = function (task) {
+    listItem = document.createElement("li");
+    checkBox = document.createElement("input");
+    label = document.createElement("label");
+    editInput = document.createElement("input");
+    editButton = document.createElement("button");
+    deleteButton = document.createElement("button");
 
-addButton.addEventListener("click", addTask);
+    checkBox.type = "checkbox";
+    editInput.type = "text";
+    editButton.innerText = "Edit";
+    editButton.className = "edit";
+    deleteButton.innerText = "Delete";
+    deleteButton.className = "delete";
+    label.innerText = task.name;
 
-for (var i = 0; i < incompleteTasksHolder.children.length; i++) {
-  bindTaskEvents(incompleteTasksHolder.children[i], taskCompleted);
-}
+    listItem.id = task.id;
+    listItem.appendChild(checkBox);
+    listItem.appendChild(label);
+    listItem.appendChild(editInput);
+    listItem.appendChild(editButton);
+    listItem.appendChild(deleteButton);
 
-for (var i = 0; i < completedTasksHolder.children.length; i++) {
-  bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
-}
+    return listItem;
+  };
+
+  const addTask = function (e) {
+    if (taskInput.value && taskInput.value.trim().length) {
+      const listItemName = taskInput.value;
+
+      const task = {
+        id: "task_" + new Date().getTime(),
+        name: listItemName,
+        inEditMode: false,
+      };
+      todoData.pending.push(task);
+      saveData();
+
+      listItem = createNewTaskElement(task);
+      incompleteTasksHolder.appendChild(listItem);
+      bindTaskEvents(listItem, taskCompleted);
+      taskInput.value = "";
+      taskInput.className = "";
+    } else {
+      taskInput.className = "error-input";
+    }
+  };
+
+  const taskChange = function () {
+    taskInput.className = "";
+  };
+
+  const getTask = function (id) {
+    console.log(todoData);
+    const pending = todoData.pending.filter((data) => data.id === id);
+    if (pending && pending.length) {
+      return pending[0];
+    }
+    const complete = todoData.completed.filter((data) => data.id === id);
+    if (complete && complete.length) {
+      return complete[0];
+    }
+  };
+
+  const getTaskIndexById = function (taskList, id) {
+    for(let i = 0; i < taskList.length; i++) {
+      if(taskList[i].id === id) {
+        return i;
+      }
+    }
+    return -1;
+  };
+
+  const editTask = function () {
+    if (this.parentNode) {
+      const listItem = this.parentNode;
+      const task = getTask(listItem.id);
+      const editInput = listItem.querySelectorAll("input[type=text")[0];
+      const label = listItem.querySelector("label");
+      const button = listItem.getElementsByTagName("button")[0];
+
+      const containsClass = listItem.classList.contains("editMode");
+      console.log(task);
+      if (containsClass) {
+        task.name = editInput.value;
+        task.inEditMode = false;
+        label.innerText = editInput.value;
+        button.innerText = "Edit";
+      } else {
+        editInput.value = label.innerText;
+        button.innerText = "Save";
+        task.inEditMode = true;
+      }
+      saveData();
+      listItem.classList.toggle("editMode");
+    }
+  };
+
+  const deleteTask = function () {
+    const listItem = this.parentNode;
+    const ul = listItem.parentNode;
+    ul.removeChild(listItem);
+
+    let index = getTaskIndexById(todoData.pending, listItem.id);
+    if(index > -1) {
+      todoData.pending.splice(index, 1);
+    }
+    index = getTaskIndexById(todoData.completed, listItem.id);
+    if(index > -1) {
+      todoData.completed.splice(index, 1);
+    }
+    saveData();
+  };
+
+  const taskCompleted = function () {
+    const listItem = this.parentNode;
+    completedTasksHolder.appendChild(listItem);
+    bindTaskEvents(listItem, taskIncomplete);
+    this.checked = false;
+    const index = getTaskIndexById(todoData.pending, listItem.id);
+    if(index > -1) {
+      const task = todoData.pending[index]
+      todoData.pending.splice(index, 1);
+      todoData.completed.push(task);
+    }
+    saveData();
+  };
+
+  const taskIncomplete = function () {
+    const listItem = this.parentNode;
+    incompleteTasksHolder.appendChild(listItem);
+    bindTaskEvents(listItem, taskCompleted);
+    this.checked = false;
+    const index = getTaskIndexById(todoData.completed, listItem.id);
+    if(index > -1) {
+      const task = todoData.completed[index]
+      todoData.completed.splice(index, 1);
+      todoData.pending.push(task);
+    }
+    saveData();
+  };
+
+  const bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
+    const checkBox = taskListItem.querySelectorAll("input[type=checkbox]")[0];
+    const editButton = taskListItem.querySelectorAll("button.edit")[0];
+    const deleteButton = taskListItem.querySelectorAll("button.delete")[0];
+    editButton.onclick = editTask;
+    deleteButton.onclick = deleteTask;
+    checkBox.onchange = checkBoxEventHandler;
+  };
+
+  addButton.addEventListener("click", addTask);
+  taskInput.addEventListener("change", taskChange);
+
+  const loadInitData = function () {
+    for (let i = 0; i < todoData.pending.length; i++) {
+      const listItem = createNewTaskElement(todoData.pending[i]);
+      incompleteTasksHolder.appendChild(listItem);
+      bindTaskEvents(listItem, taskCompleted);
+      if (todoData.pending[i].inEditMode) {
+        listItem.childNodes[3].click();
+      }
+    }
+
+    for (let i = 0; i < todoData.completed.length; i++) {
+      const listItem = createNewTaskElement(todoData.completed[i]);
+      completedTasksHolder.appendChild(listItem);
+      bindTaskEvents(listItem, taskIncomplete);
+      if (todoData.completed[i].inEditMode) {
+        listItem.childNodes[3].click();
+      }
+    }
+  };
+
+  loadInitData();
+})();
